82d42607c33f78088796f123eb1f37932539d9a8
opentech4u@gmail.com <opentech4u@gmail.com>
2020-02-19 16:17:49 +0530

    db cnfig

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/claim';
+//$config['base_url'] = 'http://localhost/claim';
 
-//$config['base_url'] = 'http://synergicportal.in/claim';
+$config['base_url'] = 'http://synergicportal.in/claim';
 
 /*
 |--------------------------------------------------------------------------
