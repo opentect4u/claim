024561b6cae88682fff54b9b880ded5cd3f2c6fc
opentech4u@gmail.com <opentech4u@gmail.com>
2020-02-19 16:13:16 +0530

    config

--- application/config/config.php
+++ application/config/config.php
@@ -25,6 +25,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config['base_url'] = 'http://localhost/claim';
 
+//$config['base_url'] = 'http://synergicportal.in/claim';
+
 /*
 |--------------------------------------------------------------------------
 | Index File
